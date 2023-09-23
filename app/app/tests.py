dde6f7b1cc68551f37fe6b2b31110c278fa69ea8
Mohamed Thiam <mohamedthiam@Mohameds-MacBook-Pro-2.local>
2023-09-23 21:43:32 +0200

    Configured docker compose and checks to wait for db(Error Fixed)

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,11 +8,11 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def testAddNumbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.sub(15,10)
+        res = calc.sub(15, 10)
 
         self.assertEqual(res, 5)
